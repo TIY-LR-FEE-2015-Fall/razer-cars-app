cb590712b2e6f37e377a0995aa7812f96118af71
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-21 15:30:53 -0500

    Inventory index skin

--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -1,5 +1,5 @@
 export default function(server) {
-  server.create('car-type', {year: 2012, manufacturer: 'Ford', modelName: 'F150', history: [1, 2, 3, 4, 5]});
+  server.create('car-type', {year: 2012, totalInventory: 10, manufacturer: 'Ford', modelName: 'F150', history: [1, 2, 3, 4, 5]});
   server.createList('car-type', 4);
   server.createList('inventory-history', 5, {car: '1'});
 }
