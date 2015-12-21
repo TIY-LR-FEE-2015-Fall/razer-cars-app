4441424c1c81a3e6ef227f2117600af48315244c
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-21 15:38:26 -0500

    Update test for currentlyAvailable

--- tests/acceptance/inventory/list-test.js
+++ tests/acceptance/inventory/list-test.js
@@ -6,7 +6,8 @@ moduleForAcceptance('Acceptance | inventory/list', {
     login();
     server.create('car-type', {year: 2012, totalInventory: 10, manufacturer: 'Ford', modelName: 'F150', history: [1, 2, 3, 4, 5]});
     server.createList('car-type', 4);
-    server.createList('inventory-history', 5, {car: '1', checkIn: new Date()});
+    server.createList('inventory-history', 1, {car: '1', checkOut: new Date(), checkIn: new Date()});
+    server.createList('inventory-history', 4, {car: '1', checkOut: new Date()});
   },
 });
 
@@ -29,6 +30,6 @@ test('visiting / shows the inventory list', function(assert) {
     assert.equal(items.length, 5, 'There should be five inventories in the list');
 
     assert.includes(firstItem.find('.inventory-list-item__description').text(), '2012 Ford F150');
-    assert.includes(firstItem.find('.inventory-list-item__available').text(), '5 Available');
+    assert.includes(firstItem.find('.inventory-list-item__available').text(), '6 Available');
   });
 });
