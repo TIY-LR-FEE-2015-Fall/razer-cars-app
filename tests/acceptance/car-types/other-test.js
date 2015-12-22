9cc997a82e3d1b3b66bcdce5edfcac4a44041d2a
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-22 11:30:14 -0500

    Update edit and others test with style

---
+++ tests/acceptance/car-types/other-test.js
@@ -0,0 +1,28 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'razer-cars-app/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | car types/other', {
+  beforeEach() {
+    login();
+  },
+});
+
+test('A user should not see the car list on /cars/new', function(assert) {
+  visit('/cars/new');
+
+  andThen(function() {
+    let items = find('.car-type-list-item');
+
+    assert.equal(items.length, 0, 'There should not be any .car-type-list-item elements on the new form');
+  });
+});
+
+test('A user should not see the car list on edit screen', function(assert) {
+  visit('/cars/1/edi');
+
+  andThen(function() {
+    let items = find('.car-type-list-item');
+
+    assert.equal(items.length, 0, 'There should not be any .car-type-list-item elements on the edit form');
+  });
+});
